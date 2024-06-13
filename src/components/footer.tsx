7a625280e48d19f1489893650eac0ef1e24729b0
eltonhas <elton_has@hotmail.com>
2024-06-13 17:25:46 -0300

    inital configuration and created footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+import Whatsapp from '@/assets/whatsapp.svg'
+
+export function Footer() {
+  return (
+    <footer className="absolute bottom-0 w-full bg-footer-background px-8 py-5 font-sans text-xs text-footer-text">
+      <div className="flex w-full max-w-7xl items-center justify-between md:mx-auto md:justify-start md:gap-3">
+        <div className="flex flex-col md:flex-row md:items-center md:gap-3">
+          <p>Sandro Luz Tattoo Shop</p>
+          <div className="hidden size-1 rounded-full bg-brand-color md:flex" />
+          <p>Rua Limoeiro, 1000 - Catalão - SP</p>
+        </div>
+
+        <div className="size-1 rounded-full bg-brand-color" />
+        <div className="flex items-center gap-0.5">
+          <Image src={Whatsapp} className="size-4" alt="whatsapp" />
+          <p>(+55 11) 99999 9999</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'var(--font-inter)',
+        londrina: 'var(--font-londrinaSolid)',
+        island: 'var(--font-islandMoments)',
+      },
+      colors: {
+        headings: '#FFFFFF',
+        typography: '#CCCCCC',
+        muted: '#636363',
+        'brand-color': '#FFD911',
+        'background-color': '#191919',
+        'footer-background': '#161616',
+        'footer-text': '#767676',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
